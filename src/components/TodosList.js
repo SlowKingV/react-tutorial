2727f2db593f50ba442970d85e61dcef9a3d0a0c
Diego Luna <slowkingvii@gmail.com>
2022-08-29 11:33:04 -0500

    Replace hardcoded IDs for UUID

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -15,7 +15,7 @@ const TodosList = ({ todos, handleChangeProps, delTodo }) => (
 );
 TodosList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
+    id: PropTypes.string,
     title: PropTypes.string,
     completed: PropTypes.bool,
   })).isRequired,
